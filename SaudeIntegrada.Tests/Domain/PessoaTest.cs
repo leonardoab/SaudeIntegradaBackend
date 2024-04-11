374525efacfe3495e61315a392788b9e09264347
Igor Lessa Robaina de Souza <igorleessa@gmail.com>
2024-04-10 23:12:52 -0300

    Ajuste testes unitarios

--- SaudeIntegrada.Tests/Domain/PessoaTest.cs
+++ SaudeIntegrada.Tests/Domain/PessoaTest.cs
@@ -1,49 +1,46 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using SaudeIntegrada.Api.Controllers;
 using SaudeIntegrada.Application.Dto;
 using SaudeIntegrada.Application.IService;
-using SaudeIntegrada.Application.Service;
-using System.Net;
-using Xunit;
+using SaudeIntegrada.Repository.Context;
 
 namespace SaudeIntegrada.Tests.Domain
 {
     [Collection("Pessoa collection")]
-    public class PessoaTest   
+    public class PessoaTest : IClassFixture<Fixture>
     {
         private readonly IPessoaService _PessoaService;
 
-        //public PessoaTest(Fixture fixture)
-        //{
-        //    _PessoaService = fixture.ServiceProvider.GetRequiredService<IPessoaService>();
-        //}
-
-        public PessoaTest(IPessoaService _PessoaService)
+        public PessoaTest(Fixture fixture)
         {
-                this._PessoaService = _PessoaService;
+            _PessoaService = fixture.ServiceProvider.GetRequiredService<IPessoaService>();
         }
+
+        private DbContextOptions<SaudeIntegradaContext> options;
+
 
         [Fact]
         public async Task DeveCriarPessoaComSucesso()
         {
             PessoaDto pessoa = new PessoaDto()
             {
-                Id = Guid.NewGuid(),
-                Nome = "João",
+                Nome = "Joao da Silva",
                 DataNascimento = DateTime.Now,
                 Sexo = "M",
-                Telefone = "21 98559-9988"
+                Telefone = "21988665544"
             };
-            
+
             var controller = new PessoaController(_PessoaService);
             var result = controller.Criar(pessoa);
 
-            Assert.True(result.ToString().Equals(HttpStatusCode.OK));
+            Assert.True(result is CreatedResult);
 
         }
 
         [Fact]
-        public void DeveEditarComSucesso()
+        public void NaoDeveCriarDuplicado()
         {
             PessoaDto pessoa = new PessoaDto()
             {
@@ -55,40 +52,21 @@ namespace SaudeIntegrada.Tests.Domain
             };
 
             var controller = new PessoaController(_PessoaService);
-            var result = controller.Editar(pessoa);
+            var result = controller.Criar(pessoa);
 
-            Assert.True(result.ToString().Equals(HttpStatusCode.OK));
-
+            Assert.True(result is CreatedResult);
         }
 
-
-        [Fact]
-        public void DeveExcluirComSucesso()
-        {
-            PessoaDto pessoa = new PessoaDto()
-            {
-                Id = new Guid("96dde085-f7d2-4997-24cc-08dc58fbaf86"),
-                Nome = "João",
-                DataNascimento = DateTime.Now,
-                Sexo = "M",
-                Telefone = "21 98559-9988",
-            };
-
-            var controller = new PessoaController(_PessoaService);
-            var result = controller.Apagar(pessoa);
-
-            Assert.True(result.ToString().Equals(HttpStatusCode.OK));
-
-        }
 
         [Fact]
         public void ListaTodos()
         {
             var controller = new PessoaController(_PessoaService);
             var result = controller.ListaTodos();
-
-            Assert.True(result.ToString().Equals(HttpStatusCode.OK));
-
+            var statusCode = (result as OkObjectResult).StatusCode;
+            //var res = (result as OkObjectResult).Value as IEnumerable<PessoaDto>;
+            
+            Assert.True(statusCode.Equals(200));
         }
 
 
@@ -99,8 +77,8 @@ namespace SaudeIntegrada.Tests.Domain
 
             var controller = new PessoaController(_PessoaService);
             var result = controller.GetPessoasPorId(id);
-
-            Assert.True(result.ToString().Equals(HttpStatusCode.OK));
+            var res = (result as OkObjectResult).Value as PessoaDto;
+            Assert.True(res is not null);
 
         }
     }
--- SaudeIntegrada.Tests/Fixture.cs
+++ SaudeIntegrada.Tests/Fixture.cs
@@ -1,26 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 using SaudeIntegrada.Application.IService;
+using SaudeIntegrada.Application.Profile;
 using SaudeIntegrada.Application.Service;
 using SaudeIntegrada.Domain.IRepository;
 using SaudeIntegrada.Repository.Context;
 using SaudeIntegrada.Repository.Repository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SaudeIntegrada.Tests
 {
     public class Fixture
     {
         public Fixture() {
+
+            var program = new Program();
+
             var serviceColletion = new ServiceCollection();
 
-            serviceColletion.AddScoped<SaudeIntegradaContext>();
+            //var connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Integrated Security=True; Initial Catalog=SaudeIntegradaDatabase";
+            
+            serviceColletion.AddDbContextFactory<SaudeIntegradaContext>(options => options.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;Integrated Security=True; Initial Catalog=SaudeIntegradaDatabase", b => b.MigrationsAssembly("SaudeIntegrada.Api")));
+            
+            serviceColletion.AddAutoMapper(typeof(PessoaProfile).Assembly);
+
+            serviceColletion.AddTransient<DbContext, SaudeIntegradaContext>();
             serviceColletion.AddTransient<IPessoaService, PessoaService>();
             serviceColletion.AddTransient<IPessoaRepository, PessoaRepository>();
+            //serviceColletion.AddTransient<IMapper, Mapper>();
 
             ServiceProvider = serviceColletion.BuildServiceProvider();
         }
