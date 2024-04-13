3076896acfb0c923ad93149c9aa52c4233501fa8
Igor Lessa Robaina de Souza <igorleessa@gmail.com>
2024-04-13 00:52:44 -0300

    .

--- SaudeIntegrada.Tests/Domain/PessoaTest.cs
+++ SaudeIntegrada.Tests/Domain/PessoaTest.cs
@@ -1,38 +1,38 @@
-﻿using Microsoft.AspNetCore.Connections.Features;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Connections.Features;
 using Microsoft.Extensions.DependencyInjection;
 using SaudeIntegrada.Application.Dto;
 using SaudeIntegrada.Application.IService;
 using SaudeIntegrada.Application.Service;
+using SaudeIntegrada.Domain.Domains;
+using SaudeIntegrada.Domain.IRepository;
+using SaudeIntegrada.Repository.Repository;
+using System.Net.WebSockets;
 
 namespace SaudeIntegrada.Tests.Domain
 {
     [Collection("Pessoa collection")]
     public class PessoaTest : IClassFixture<DependencyInjection>
     {
+        private readonly IPessoaRepository _PessoaRepository;
+        private readonly IContaRepository _ContaRepository;
         private readonly IPessoaService _PessoaService;
         private readonly IContaService _ContaService;
+        private IMapper mapper;
 
         public PessoaTest(DependencyInjection dependencyInjection)
         {
-            _PessoaService = dependencyInjection.ServiceProvider.GetRequiredService<IPessoaService>();
             _ContaService = dependencyInjection.ServiceProvider.GetRequiredService<IContaService>();
+            _PessoaService = dependencyInjection.ServiceProvider.GetRequiredService<IPessoaService>();
+            _PessoaRepository = dependencyInjection.ServiceProvider.GetRequiredService<IPessoaRepository>();
+            _ContaRepository = dependencyInjection.ServiceProvider.GetRequiredService<IContaRepository>();
+
         }
 
         [Fact]
         public void DeveCriarPessoaComSucesso()
         {
-            var listConta = _ContaService.ObterTodos();
-            var objConta = listConta.FirstOrDefault();
-
-            PessoaCriarDto pessoa = new PessoaCriarDto()
-            {
-                Nome = $"Pessoa {utils.RandomString(6)}",
-                DataNascimento = DateTime.Now,
-                Sexo = "M",
-                ContaId = objConta.Id
-            };
-
-            var result = _PessoaService.Criar(pessoa);
+            var result = CriarPessoa();
             Assert.True(result is not null);
         }
 
@@ -55,7 +55,7 @@ namespace SaudeIntegrada.Tests.Domain
 
             var result = _PessoaService.Editar(pessoa);
             Assert.True(result is not null);
-            
+
         }
 
 
@@ -90,9 +90,44 @@ namespace SaudeIntegrada.Tests.Domain
         {
             var resultList = _PessoaService.ObterTodos();
             var objPessoa = resultList.FirstOrDefault();
-            var result =  _PessoaService.Obter(objPessoa.Id);
+            var result = _PessoaService.Obter(objPessoa.Id);
             Assert.True(result is not null);
 
         }
+
+        public Pessoa CriarPessoa()
+        {
+            var conta = CriarConta();
+
+            Pessoa pessoa = new Pessoa()
+            {
+                Nome = $"Pessoa {utils.RandomString(6)}",
+                DataNascimento = DateTime.Now,
+                Sexo = "M",
+                ContaId = conta.Id,
+                Conta = conta,
+                AvaliacaoFichas = new List<AvaliacaoFicha>()
+            };
+           
+            this._PessoaRepository.Save(pessoa);
+            return pessoa;
+        }
+
+        public Conta CriarConta()
+        {
+            Conta conta = new Conta()
+            {
+                Email = $"email_{utils.RandomString(4)}@gmail.com",
+                Password = "123456",
+                Telefone = $"{utils.RandomNumber(11)}",
+                Apelido = $"apelido_{utils.RandomString(4)}",
+                Ativo = true
+            };
+
+            this._ContaRepository.Save(conta);
+
+            return conta;
+        }
+
     }
 }
